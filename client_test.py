79ac602e1f470df5ceb177fcfff1150367e84935
Armand <armandwinant@googlemail.com>
2024-05-05 16:42:51 +0200

    add unit tests for getDataPoint function

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    _, _, _, price = getDataPoint(quotes[0])
+    self.assertEqual((121.2 + 120.48) / 2, price)
+
+    _, _, _, price = getDataPoint(quotes[1])
+    self.assertEqual((121.68 + 117.87) / 2, price)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +20,28 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    _, bid_price, ask_price, _ = getDataPoint(quotes[0])
+    self.assertGreaterEqual(bid_price, ask_price)
+
+    _, bid_price, ask_price, _ = getDataPoint(quotes[1])
+    self.assertGreaterEqual(bid_price, ask_price)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_zeroBidPrice(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    prices = {}
+    for quote in quotes:
+      stock, _, _, price = getDataPoint(quote)
+      prices[stock] = price
+    self.assertEqual(prices["DEF"], 0)
+    ratio = getRatio(prices["ABC"], prices["DEF"])
+
+    self.assertIsNone(ratio)
 
 
 
